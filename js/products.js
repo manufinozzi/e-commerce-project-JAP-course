25ba4b691bf9ac2fabc55f85ef0b9705a6b00ce3
manufinozzi <juanmfinozzi93@gmail.com>
2021-08-12 14:18:50 -0300

    Moved getJSONData from init.js to products.js, added div to products.html

--- js/products.js
+++ js/products.js
@@ -2,5 +2,18 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+    getJSONData(PRODUCTS_URL).then(function(resultObj) {
+        if (resultObj.status === 'ok') {
+            Productsarray = resultObj.data;
+        }
+    })
 });
+
+/*
+Para la entrega número 1:
+
+1- Obtener la información. Para eso utilizar FETCH o la función GETJSONDATA definida en init.js
+2- Hacer procedimiento para insertar en HTML el primer producto
+3- Hacer procedimiento en HTML para insertar todos los productos
+*/
+
